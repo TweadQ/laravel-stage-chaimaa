e63f2d80b9ec22f0a8fd3778277db7a2f6bc8564
TweadQ <quentinlaisney@hotmail.fr>
2022-12-15 02:50:25 +0100

    suppression de qq commentaires

--- resources/views/components/cards/cardIndexClothes.blade.php
+++ resources/views/components/cards/cardIndexClothes.blade.php
@@ -1,15 +1,5 @@
 @props(['url_img', 'title'])
-{{-- <div class="flex flex-col items-center">
-    <img class="w-[38rem] h-[40rem] relative"
-        alt="{{ $title }}"
-        src="{{ $url_img }}"
-    >
-    <div class="p-4 absolute pt-[18rem] pr-10">
-            <a href="" class="pb-3 text-[5rem] font-black uppercase text-teal-700">{{ $title }}</a>
-    </div>
-</div> --}}
 <div class="pt-2 w-[19.5rem] h-[20.5rem] card bg-base-100 shadow-xl image-full
-    {{-- 2xl:w-[38.1rem] 2xl:h-[40.5rem] --}}
     xl:w-[28rem] xl:h-[30rem]
     lg:w-[18rem] lg:h-[20rem]
     md:w-[13.5rem] md:h-[15.5rem]
--- resources/views/components/cards/cardIndexClothes2.blade.php
+++ resources/views/components/cards/cardIndexClothes2.blade.php
@@ -1,15 +1,5 @@
 @props(['url_img', 'title'])
-{{-- <div class="flex flex-col items-center">
-    <img class="w-[38rem] h-[40rem] relative"
-        alt="{{ $title }}"
-        src="{{ $url_img }}"
-    >
-    <div class="p-4 absolute pt-[18rem] pr-10">
-            <a href="" class="pb-3 text-[5rem] font-black uppercase text-teal-700">{{ $title }}</a>
-    </div>
-</div> --}}
 <div class="pt-2 w-[19.5rem] h-[20.5rem] card image-full 
-  {{-- 2xl:w-[57.5rem] 2xl:h-[45rem] --}}
   xl:w-[42.3rem] xl:h-[34.5rem]
   lg:w-[27.3rem] lg:h-[28.5rem]
   md:w-[20.5rem] md:h-[20.5rem]
