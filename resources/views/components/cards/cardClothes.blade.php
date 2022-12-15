8ca62876aaaf4469b2cfbae8c45a1c7b0681d39f
TweadQ <quentinlaisney@hotmail.fr>
2022-12-15 02:48:42 +0100

    rectif pb grid + responsive page show

--- resources/views/components/cards/cardClothes.blade.php
+++ resources/views/components/cards/cardClothes.blade.php
@@ -1,10 +1,8 @@
 @props(['url_img', 'title'])
-<div class="">
-    <img class="w-[20rem] h-[22rem]"
+<div class="flex flex-col items-center">
+    <img class="w-[20rem] h-[22rem] pb-4"
         alt="{{ $title }}"
         src="{{ asset('storage/' . $url_img) }}"
     >
-    <div class="text-center">
-            <a href="" class="pb-3 text-2xl font-black uppercase text-teal-700">{{ $title }}</a>
-    </div>
+    <p class="pb-3 text-2xl font-black uppercase text-teal-700">{{ $title }}</p>
 </div>
